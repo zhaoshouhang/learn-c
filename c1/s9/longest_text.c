9549e11bf69e0d5c20aec4c5697c628596dde56c
hansel zhao <zhaoshouhang9295@icloud.com>
2024-06-12 17:37:10 +0800

    fix error

--- c1/s9/longest_text.c
+++ c1/s9/longest_text.c
@@ -3,33 +3,34 @@
 
 #define MAXLINE 1000 /* 输入行的最大长度 */
 
-int getline(char line[], int maxline);
+int get_line(char line[], int maxline);
 
 void copy(char to[], char from[]);
 
-main()
+int main()
 {
-    int len;                // 当前行长度
-    int max;                // 目前最长行的长度
-    char line[MAXLINE];     // 当前输入行
-    char longtest[MAXLINE]; // 保存最长行
+
+    int len;               // 当前行长度
+    int max;               // 目前最长行的长度
+    char line[MAXLINE];    // 当前输入行
+    char longest[MAXLINE]; // 保存最长行
 
     max = 0;
-    while ((len - getline(line, MAXLINE)) > 0)
+    while ((len = get_line(line, MAXLINE)) > 0)
     {
         if (len > max)
         {
             max = len;
-            copy(longtest, line);
+            copy(longest, line);
         }
     }
     if (max > 0)
     {
-        printf("%s", longtest);
+        printf("%s", longest);
     }
 }
 
-int getline(char s[], int lim)
+int get_line(char s[], int lim)
 {
     int c, i;
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
