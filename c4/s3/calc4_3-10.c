258fa858e19eac3d8077614c6f6f12e64e7ae41a
hansel zhao <zhaoshouhang9295@icloud.com>
2024-08-07 17:36:17 +0800

    calc program

---
+++ c4/s3/calc4_3-10.c
@@ -0,0 +1,244 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <string.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+
+// 4-5 define
+#define SIN '1'
+#define EXP '2'
+#define POW '3'
+
+int getop(char[]);
+void push(double);
+double pop(void);
+
+int main()
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF)
+    {
+        switch (type)
+        {
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0.0)
+            {
+                push(pop() / op2);
+            }
+            else
+            {
+                printf("error zero divisor\n");
+            }
+            break;
+        // 习题4-3 支持取模
+        case '%':
+            op2 = pop();
+            if (op2 != 0.0)
+            {
+                push((int)pop() % (int)op2);
+            }
+            else
+            {
+                printf("error zero divisor\n");
+            }
+            break;
+        // 习题4-5 支持math中的部分函数
+        case SIN:
+            push(sin(pop()));
+            break;
+        case EXP:
+            push(exp(pop()));
+            break;
+        case POW:
+            op2 = pop();
+            push(pow(pop(), op2));
+            break;
+        case '\n':
+            printf("\t%.8g\n", pop());
+            break;
+        default:
+            printf("error: unknown command %s\n", s);
+            break;
+        }
+    }
+}
+
+int sp = 0;
+double val[MAXOP];
+
+void push(double f)
+{
+    if (sp < MAXOP)
+    {
+        val[sp++] = f;
+    }
+    else
+    {
+        printf("error : stack full , can't push %g \n", f);
+    }
+}
+double pop(void)
+{
+    if (sp > 0)
+    {
+        return val[--sp];
+    }
+    else
+    {
+        printf("error: stack empty\n");
+    }
+}
+// 习题4-4
+/*
+    打印栈顶元素
+*/
+void print_stack_top()
+{
+    if (sp > 0)
+    {
+        printf("top:%g\n", val[sp - 1]);
+    }
+    else
+    {
+        printf("error: stack empty\n");
+    }
+}
+
+double cur_stack_top;
+void copy_stack_top()
+{
+    if (sp > 0)
+    {
+        cur_stack_top = val[sp - 1];
+    }
+    else
+    {
+        printf("error: stack empty\n");
+    }
+}
+void exchange_stack_element()
+{
+    if (sp <= 1)
+    {
+        printf("not enough elements\n");
+    }
+    else
+    {
+        double t = val[sp - 1];
+        val[sp - 1] = val[sp - 2];
+        val[sp - 2] = t;
+    }
+}
+void clear_stack()
+{
+    for (int i = 0; i < sp; i++)
+    {
+        val[i] = 0.0;
+    }
+    sp = 0;
+}
+//----------------
+int getch(void);
+void ungetch(int);
+
+int getop(char s[])
+{
+    int i, c;
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+    {
+        ;
+    }
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+    {
+        // 4-5 增加特殊函数sin exp pow
+        if (isalpha(c))
+        {
+            s[0] = tolower(s[0]);
+            int i = 0;
+            while (isalpha(s[++i] = c = getch()))
+            {
+                s[i] = tolower(s[i]);
+
+                s[i + 1] = '\0';
+                if (strcmp(s, "sin") == 0)
+                {
+                    return SIN;
+                }
+                else if (strcmp(s, "exp") == 0)
+                {
+                    return EXP;
+                }
+                else if (strcmp(s, "pow") == 0)
+                {
+                    return POW;
+                }
+            }
+        }
+        return c;
+    }
+    i = 0;
+    if (isdigit(c))
+    {
+        while (isdigit(s[++i] = c = getch()))
+        {
+            ;
+        }
+    }
+    if (c == '.')
+    {
+        while (isdigit(s[++i] = c = getch()))
+        {
+            ;
+        }
+    }
+    s[i] = '\0';
+    if (c != EOF)
+    {
+        ungetch(c);
+    }
+    return NUMBER;
+}
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE)
+    {
+        printf("ungetch:  too many characters\n");
+    }
+    else
+    {
+        buf[bufp++] = c;
+    }
+}
