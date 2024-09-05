d80480b52af9c65d8c7bcb2c8bc7dbf283c81dc7
hansel zhao <zhaoshouhang9295@icloud.com>
2024-09-05 16:29:26 +0800

    5.2

--- c5/s2/getint.c
+++ c5/s2/getint.c
@@ -3,7 +3,7 @@
 
 int getch(void);
 void ungetch(int);
-
+/* 把输入中的下一个整型数据赋值给*pn */
 int getint(int *pn)
 {
     int c, sign;
@@ -11,17 +11,28 @@ int getint(int *pn)
     while (isspace(c = getch()))
     {
     }
-    if (isdigit(c) && c != EOF && c != '+' && c != '-')
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-')
     {
-        ungetch(c);
+        ungetch(c); // 输入的不是一个数字
         return 0;
     }
     sign = (c == '-') ? -1 : 1;
 
     if (c == '+' || c == '-')
     {
-        c = getch();
+        int nextc = getch();
+        if (isdigit(nextc))
+        {
+            c = nextc;
+        }
+        else
+        {
+            ungetch(nextc);
+            ungetch(c);
+            return 0;
+        }
     }
+
     for (*pn = 0; isdigit(c); c = getch())
     {
         *pn = 10 * *pn + (c - '0');
@@ -58,7 +69,7 @@ void ungetch(int c)
 
 int main()
 {
-    int a = 1;
+    int a;
     int b = getint(&a);
 
     printf("a = %d,b = %d\n", a, b);
