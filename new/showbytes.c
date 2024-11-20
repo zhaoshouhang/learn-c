685c27a1dbcd74c45fee750f3a1b1f1e4f1c7ffb
hansel zhao <zhaoshouhang9295@icloud.com>
2024-11-20 17:36:21 +0800

    tip

--- new/showbytes.c
+++ new/showbytes.c
@@ -27,6 +27,14 @@ void show_pointer(void *x)
     show_bytes((byte_pointer)&x, sizeof(void *));
 }
 
+int func1(unsigned word)
+{
+    return (int)((word << 24) >> 24);
+}
+int func2(unsigned word)
+{
+    return ((int)word << 24) >> 24;
+}
 int main()
 {
     int val = 0x87654321;
@@ -38,4 +46,18 @@ int main()
     show_bytes((byte_pointer)str, 6);
     const char *s = "abcdef";
     show_bytes((byte_pointer)s, strlen(s));
+
+    unsigned int a = -1;
+
+    printf("%u\n", a);
+    show_bytes((byte_pointer)(&a), sizeof(int));
+    printf("=========位移运算======\n");
+    printf("0x00000076:func1:%x\n", func1(0x00000076));
+    printf("0x00000076:func2:%x\n", func2(0x00000076));
+    printf("0x87654321:func1:%x\n", func1(0x87654321));
+    printf("0x87654321:func2:%x\n", func2(0x87654321));
+    printf("0x000000C9:func1:%x\n", func1(0x000000C9));
+    printf("0x000000C9:func2:%x\n", func2(0x000000C9));
+    printf("0xEDCBA987:func1:%x\n", func1(0xEDCBA987));
+    printf("0xEDCBA987:func2:%x\n", func2(0xEDCBA987));
 }
